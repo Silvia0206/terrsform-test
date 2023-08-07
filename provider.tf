74f3fc801e18b26f979f2dfecceb498bf7ef1514
Silvia0206 <87439700+Silvia0206@users.noreply.github.com>
2023-08-07 19:48:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,5 +9,5 @@ provider "google" {
   project     = "terraform-learn-389307"
   region      = "us-central1"
   zone        = "us-central1-c"
-  credentials = "C:\\terraform_1.4.6_windows_386\\terraform-learn-key.json"
+
 }
