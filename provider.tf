21c5d996b8fdc6008dd8064933471e7e7cb427ac
Silvia0206 <87439700+Silvia0206@users.noreply.github.com>
2023-10-11 00:51:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+        source = "hashicorp/google"
     }
   }
 }
-provider "google" {
-  project     = "terraform-new-399715"
-  region      = "us-central1"
-  zone        = "us-central1-c"
 
+provider "google" {
+    project = "terraform-new-399715"
+    region = "us-central1" 
+    zone = "us-central1-c"
 }
